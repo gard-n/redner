87b91155f249f1341c84ef4f7d62264c07b082ea
Niklas Gard <NiklasGard@gmx.de>
2025-05-22 16:48:49 +0200

    Update setup.py
    Remove automatic gpu detection. Set cpu as default unless REDNER_CUDA is set.

--- setup.py
+++ setup.py
@@ -97,15 +97,15 @@ packages = []
 build_with_cuda = False
 if torch_spec is not None:
     packages.append('pyredner')
-    import torch
-    if torch.cuda.is_available():
-        build_with_cuda = True
+    # import torch
+    # if torch.cuda.is_available():
+    #     build_with_cuda = True
 if tf_spec is not None and sys.platform != 'win32':
     packages.append('pyredner_tensorflow')
-    if not build_with_cuda:
-        import tensorflow as tf
-        if tf.test.is_gpu_available(cuda_only=True, min_cuda_compute_capability=None):
-            build_with_cuda = True
+    # if not build_with_cuda:
+    #     import tensorflow as tf
+    #     if tf.test.is_gpu_available(cuda_only=True, min_cuda_compute_capability=None):
+    #         build_with_cuda = True
 if len(packages) == 0:
     print('Error: PyTorch or Tensorflow must be installed. For Windows platform only PyTorch is supported.')
     exit()
